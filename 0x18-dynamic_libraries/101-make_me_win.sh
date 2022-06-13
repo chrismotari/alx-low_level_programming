79a38ba2504dbe00ca502c6b7a2cb370886e2294
ChrisM <chrismotari@gmail.com>
2022-06-13 19:36:14 +0300

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/holbertonschool/0x18.c.git/101-md5_gm
+wget -P /tmp https://github.com/nevillionaire/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
